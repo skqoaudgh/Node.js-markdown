2323cb95f9d35687c763487dc81b962007c9dad3
myungho <skqoaudgh2@hanmail.net>
2019-07-14 15:24:25 +0900

    디렉터리 간단화. 결과 페이지 추가

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const markdown = require('markdown').markdown;
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res, next) => {
     res.render('index.ejs');
+});
+
+app.post('/output', (req, res, next) => {
+    const html_content = markdown.toHTML(req.body.inputTextarea);
+    res.send(html_content);
 });
 
 app.listen(3000, () => {
