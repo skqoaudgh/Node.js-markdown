97fdcbed1fed44f4aae5968556faff760f7da850
myungho <skqoaudgh2@hanmail.net>
2019-07-16 12:55:08 +0900

    뒤로가기 버튼 추가. 뒤로 이동 시, 이전에 작성한 코드가 그대로 유지

--- public/output.js
+++ public/output.js
@@ -1,6 +1,11 @@
 const tempDoc = document.getElementById('tempResult-doc');
 const outputDoc = document.getElementById('output-doc');
 const outputHTML = document.getElementById('output-html');
+const backBtn = document.getElementById('back');
 
 outputDoc.innerHTML = tempDoc.value;
 outputHTML.innerHTML = outputHTML.value;
+
+backBtn.addEventListener('click', (event) => {
+    location.href = "/";
+});
