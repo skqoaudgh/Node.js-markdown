e3e30203d393fce570b5dea84e3f7f215650f825
myungho <skqoaudgh2@hanmail.net>
2019-07-16 13:24:59 +0900

    마크다운 문서 다운로드 기능 추가

--- public/output.js
+++ public/output.js
@@ -2,9 +2,12 @@ const tempDoc = document.getElementById('tempResult-doc');
 const outputDoc = document.getElementById('output-doc');
 const outputHTML = document.getElementById('output-html');
 const backBtn = document.getElementById('back');
+const downloadBtn = document.getElementById('download');
 
 outputDoc.innerHTML = tempDoc.value;
 outputHTML.innerHTML = outputHTML.value;
+
+downloadBtn.href = window.URL.createObjectURL(new Blob([localStorage.getItem('markdown')], {type: 'text/markdown'}));
 
 backBtn.addEventListener('click', (event) => {
     location.href = "/";
